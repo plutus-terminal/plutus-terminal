f995e559503b53e62f40fed711c70619d471d1e5
Plutus <plutus-terminal@proton.me>
2024-09-23 09:04:13 -0400

    Remove code related to CEX

--- plutus_terminal/core/exchange/valid_exchanges.py
+++ plutus_terminal/core/exchange/valid_exchanges.py
@@ -1,6 +1,5 @@
 """Dict of valid exchanges."""
 
-from plutus_terminal.core.exchange.bitget.exchange import BitgetExchange
 from plutus_terminal.core.exchange.foxify.exchange import FoxifyExchange
 from plutus_terminal.core.exchange.foxify.funded_exchange import FoxifyFundedExchange
 
