80e64d65afee78d6e36c1737ebbf84e0062223e5
Plutus <plutus-terminal@proton.me>
2025-04-16 22:29:41 -0400

    [fix] TreeOfAlphaNews does not return message after login anymore

--- plutus_terminal/core/news/tree_news.py
+++ plutus_terminal/core/news/tree_news.py
@@ -101,11 +101,6 @@ class TreeNews(NewsFetcher):
         if not self._socket:
             return
         await self._socket.send(f"login {tree_api_key}")
-        login_attempt = await self._socket.recv()
-        login_attempt = json.loads(login_attempt)
-        login_attempt.setdefault("user", {})
-        login_attempt["user"].pop("address", None)
-        LOGGER.info("TreeOfAlpha login result: %s", login_attempt)
 
     @retry(
         wait=wait_exponential(multiplier=1, min=0.4, max=2),
