f2e1b2857620bfa20a900bd9508bc7bd8344ff68
PlutusTerminal <plutus-terminal@proton.me>
2026-03-13 08:25:04 -0400

    Remove old foxify implementation

--- plutus_terminal/core/exchange/valid_exchanges.py
+++ plutus_terminal/core/exchange/valid_exchanges.py
@@ -1,7 +1,5 @@
 """Dict of valid exchanges."""
 
-from plutus_terminal.core.exchange.foxify.exchange import FoxifyExchange
-from plutus_terminal.core.exchange.foxify.funded_exchange import FoxifyFundedExchange
 from plutus_terminal.core.exchange.orderly.exchange import OrderlyExchange
 
 VALID_EXCHANGES = {
